b56dad7bef15d6c2d5588c647ea0f12f87982308
Diego Saavedra <dmsaavedra88@gmail.com>
2024-05-27 03:44:31 -0500

    add changes

--- main.py
+++ main.py
@@ -3,16 +3,10 @@ import os
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    my_set = {"Esto es un mensaje modificado!"}
-    return jsonify(list(my_set))
-
-@app.route('/my_name')
-def my_name():
     my_set = {"Hello World my name is: Diego Saavedra"}
     return jsonify(list(my_set))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5001))
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
